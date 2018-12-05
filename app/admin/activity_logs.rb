5f3e120b532e4d46c309ca62587a976a20a16a6c
Raghu Betina <raghu@firstdraft.com>
2018-12-05 03:44:29 +0000

    Whitelist columns in ActivityLog for ActiveAdmin

--- app/admin/activity_logs.rb
+++ app/admin/activity_logs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ActivityLog do
+
+ permit_params :user_name, :action, :activity_description, :file_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
